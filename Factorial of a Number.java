32cd477727450022b904c67b263fe764f6015da0
Minna Joby <24mp2277@rit.ac.in>
2025-06-28 14:13:38 +0530

    Create  Factorial of a Number.java

---
+++ Factorial of a Number.java
@@ -0,0 +1,15 @@
+import java . util . Scanner ;
+public class Factorial {
+   public static void main ( String [] args ) {
+     Scanner scanner = new Scanner ( System . in ) ;
+     System . out . print (" Enter a number : ") ;
+     int number = scanner . nextInt () ;
+     int factorial = 1;
+     int i = 1;
+     while ( i <= number ) {
+     factorial *= i ;
+     i ++;
+     }
+     System . out . println ("The factorial of " + number + " is:" + factorial ) ;
+ }
+}
