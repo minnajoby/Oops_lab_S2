063cd52fbab6470b8680e2e07cca72c8e74e161c
Minna Joby <24mp2277@rit.ac.in>
2025-06-28 14:35:36 +0530

    Create Rectangle.java

---
+++ Graphics Package for Geometric Figures/Rectangle.java
@@ -0,0 +1,16 @@
+package graphics;
+
+public class Rectangle implements GeometricFigure {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+}
